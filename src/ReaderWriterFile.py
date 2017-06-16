d9ef09a2e14a19cdf5f724873151806160ef082e
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-06-16 18:31:22 +0200

    Creating project

---
+++ src/ReaderWriterFile.py
@@ -0,0 +1,6 @@
+'''
+Created on Jun 16, 2017
+Utility class to read the contents of a file in to an ArrayList of Strings.
+Also write back to file the content in an ArrayList of Strings.
+@author: chris
+'''
